72055f7dc597b2fafced36904775065782be9d00
tspetrov <tsvetomir.miroslavov@gmail.com>
2013-04-15 19:26:24 +0300

    Update Main.java

--- bar/src/Common/Main.java
+++ bar/src/Common/Main.java
@@ -6,7 +6,8 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-	  	// TODO Auto-generated method stubtry
+	  	// TODO Auto-generated method stubtry try this
+	  	
 
 		
 	}
